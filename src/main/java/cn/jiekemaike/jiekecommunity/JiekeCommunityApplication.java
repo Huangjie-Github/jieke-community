b9d202924c0fa2cbf04abfc271c94680437602d2
jiekemaike <1728537059@qq.com>
2020-01-21 21:03:23 +0800

    fist commit

---
+++ src/main/java/cn/jiekemaike/jiekecommunity/JiekeCommunityApplication.java
@@ -0,0 +1,13 @@
+package cn.jiekemaike.jiekecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiekeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiekeCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/jiekemaike/jiekecommunity/JiekeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jiekemaike.jiekecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiekeCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
