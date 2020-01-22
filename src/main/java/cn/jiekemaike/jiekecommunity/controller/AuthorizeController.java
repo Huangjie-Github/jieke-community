0ed15f3b4edede88a9726a03bb3dc4a5aeefcc2a
jiekemaike <1728537059@qq.com>
2020-01-23 01:13:00 +0800

    2020-01-23 01:12

--- src/main/java/cn/jiekemaike/jiekecommunity/controller/AuthorizeController.java
+++ src/main/java/cn/jiekemaike/jiekecommunity/controller/AuthorizeController.java
@@ -14,7 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.UUID;
@@ -38,7 +40,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setCode(code);
@@ -47,16 +50,18 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirect_uri);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = gitHubProvider.getUser(accessToken);
+
         if (gitHubUser!=null){
             //登陆成功
-            request.getSession().setAttribute("user",gitHubUser);
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitHubUser.getName());
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             //登陆失败
--- src/main/java/cn/jiekemaike/jiekecommunity/mapper/UserMapper.java
+++ src/main/java/cn/jiekemaike/jiekecommunity/mapper/UserMapper.java
@@ -3,10 +3,17 @@ package cn.jiekemaike.jiekecommunity.mapper;
 import cn.jiekemaike.jiekecommunity.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import javax.naming.Name;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into User(name,account_id,token,gmt_create,gmt_modified) value(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from User where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
