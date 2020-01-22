0fbd310fd3facb9eec9443f65f678b6b5c15e360
jiekemaike <1728537059@qq.com>
2020-01-22 10:17:35 +0800

    add

--- src/main/java/cn/jiekemaike/jiekecommunity/controller/AuthorizeController.java
+++ src/main/java/cn/jiekemaike/jiekecommunity/controller/AuthorizeController.java
@@ -6,6 +6,8 @@ import cn.jiekemaike.jiekecommunity.provider.GitHubProvider;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,20 +15,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.IOException;
 
 @Controller
+@PropertySource("classpath:application.properties")
 public class AuthorizeController {
 
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("b8785f06838f19e663ce");
+        accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_secret("f5b4d1bebd542498616ff427de8d96ad51f6d2d9");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_secret(client_secret);
+        accessTokenDTO.setRedirect_uri(redirect_uri);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
 
